5deb5818744a22a4bc367a359f6f9342340d6f77
Zahid <hzahidnaeem@gmail.com>
2019-07-26 02:23:29 +0500

    FormControl addes as commented text

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -189,6 +189,161 @@ class Item extends Component {
             </>
         );
     }
+    /*
+import React, { Component } from 'react';
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+
+class Item extends Component {
+
+    state = {
+        item: {},
+        navigationDtl: {}
+    }
+
+    componentDidMount() {
+        const axios = require('axios');
+        axios.get('http://localhost:8089/item/first')
+            .then(res => {
+                const { item, navigationDtl } = res.data;
+                this.setState({ item, navigationDtl })
+            })
+            .catch(console.log)
+    }
+
+    handleItemChange = (event) => {
+        console.log(event.target.value);
+        let item = this.state.item;
+        item[event.target.name] = event.target.value;
+        this.setState({ item });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+    }
+
+
+    render() {
+        let obj = this.state.item;
+        return (
+            <>
+                <center><h1>Item Registrtion Form</h1></center>
+                <div>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Row>
+                        <Form.Group ><Form.Label>Item Code</Form.Label>
+                            <FormControl
+                                readOnly
+                                plaintext
+                                defaultValue={obj.itemCode}
+                            />
+                        </Form.Group>
+
+                        <Form.Group ><Form.Label>Item Barcode</Form.Label>
+                            <FormControl
+                                placeholder="Item Barcode"
+                                aria-label="Item Barcode"
+                                aria-describedby="basic-addon2"
+                                defaultValue={obj.itemBarcode}
+                                onChange={this.handleItemChange}
+                            />
+                        </Form.Group>
+
+                        <Form.Group ><Form.Label>Item Desc.</Form.Label>
+                            <FormControl
+                                placeholder="Item Desc."
+                                aria-label="Item Desc."
+                                defaultValue={obj.itemDesc}
+                                onChange={this.handleItemChange}
+                            />
+                        </Form.Group>
+                        </Form.Row>
+                        <Form.Group ><Form.Label>Item Category</Form.Label>
+                            <FormControl
+                                placeholder="Item Category"
+                                aria-label="Item Category"
+
+                                defaultValue={obj.itemCategory}
+                                onChange={this.handleItemChange}
+                            />
+                        </Form.Group>
+
+                        <Form.Group ><Form.Label>Item U.O.M</Form.Label>
+                            <FormControl
+                                placeholder="Item U.O.M"
+                                aria-label="Item U.O.M"
+                                defaultValue={obj.itemUom}
+                                onChange={this.handleItemChange}
+                            />
+                        </Form.Group>
+
+                        <Form.Group ><Form.Label>Purchase Price</Form.Label>
+                            <FormControl
+                                placeholder="Purchase Price"
+                                aria-label="Purchase Price"
+                                defaultValue={obj.purchasePrice}
+                                onChange={this.handleItemChange}
+                            />
+                        </Form.Group>
+
+                        <Form.Group ><Form.Label>Sale Price</Form.Label>
+                            <FormControl
+                                placeholder="Sale Price"
+                                aria-label="Sale Price"
+                                defaultValue={obj.salePrice}
+                                onChange={this.handleItemChange}
+                            />
+                        </Form.Group>
+
+                        <Form.Group ><Form.Label>Min. Stock</Form.Label>
+                            <FormControl
+                                placeholder="Min. Stock"
+                                aria-label="Min. Stock"
+                                defaultValue={obj.minStock}
+                                onChange={this.handleItemChange}
+                            />
+                        </Form.Group>
+
+                        <Form.Group ><Form.Label>Max. Stock</Form.Label>
+                            <FormControl
+                                placeholder="Max. Stock"
+                                aria-label="Max. Stock"
+                                defaultValue={obj.maxStock}
+                                onChange={this.handleItemChange}
+                            />
+                        </Form.Group>
+
+                        <Form.Group ><Form.Label>Effective Start Date</Form.Label>
+                            <FormControl
+                                placeholder="Effective Start Date"
+                                aria-label="Effective Start Date"
+                                defaultValue={obj.effectiveStartDate}
+                                onChange={this.handleItemChange}
+                            />
+                        </Form.Group>
+
+                        <Form.Group ><Form.Label>Effective End Date</Form.Label>
+                            <FormControl
+                                placeholder="Effective End Date"
+                                aria-label="Effective End Date"
+                                defaultValue={obj.effectiveEndDate}
+                                onChange={this.handleItemChange}
+                            />
+                        </Form.Group>
+
+                        <Button type="submit" variant="primary" size="lg" block active>Submit</Button>
+                    </Form>
+                </div>
+            </>
+        );
+    }
+}
+
+export default Item;
+    */
 }
 
 export default Item;
