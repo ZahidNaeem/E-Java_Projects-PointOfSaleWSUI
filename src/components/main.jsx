09456eb3a51b29ae0a2ecf4ac7a03288e3e4cf62
Furqan Ahmad <furqanahmad21@hotmail.com>
2019-08-04 23:56:34 +0500

    WIP: Added new main page

---
+++ src/components/main.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Main extends Component {
+    state = {  }
+    render() { 
+        return ( <>
+            <h2>Main Page</h2>
+        </> );
+    }
+}
+ 
+export default Main;
